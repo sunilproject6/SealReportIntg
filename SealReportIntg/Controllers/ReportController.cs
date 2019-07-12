d2b5dec7bda70106e3f1a3bfdc63b0bfd96d7a1e
sunil <sunilproject6@gmail.com>
2019-07-12 16:45:51 +0530

    commit

---
+++ SealReportIntg/Controllers/ReportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace SealReportIntg.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult ShowReport()
+        {
+            string url = ConfigurationManager.AppSettings["SealUrl"].ToString();
+            string report = ConfigurationManager.AppSettings["ReportPath"].ToString();
+            ViewBag.SealUrl = "'" + url + "'";
+            ViewBag.Report = "'" + report + "'";
+            return View();
+        }
+
+    }
+}
---
+++ SealReportIntg/Scripts/Custom/ShowReport.js
@@ -0,0 +1,33 @@
+﻿
+path = Report;
+
+sealServer = ReportUrl;
+render = false;
+viewGUID = ''
+outputGUID = ''
+
+$("#bid").click(function () {
+
+    function executeReport() {
+
+        $.post(sealServer + "/SWILogin", {
+            user: "userName", // The user name
+            password: "password" // The password
+        })
+            .done(function (data) {
+                var f = $('<form method="post" target="reportIframe" />').appendTo('body');
+                f.attr('action', sealServer + "/SWExecuteReport");
+                f.append($('<input>').attr('name', 'path').attr('value', path));  //the report path,  e.g. "/Samples/01-Simple list (Products).srex"
+                f.children('input').attr('type', 'hidden');
+                f.submit();
+            });
+        return false;
+
+    }
+
+    executeReport("/" + path);
+});
+
+
+
+
